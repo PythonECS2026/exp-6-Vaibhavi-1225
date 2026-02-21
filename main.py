c2e950423e1c70eebcbfc5449acc57cca844b1b4
Vaibhavi-1225 <vaibhavihaveri1@gmail.com>
2026-02-21 17:51:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 # AIM: WAP to find students appearing for entrance exams
 # Coder:
 # Date:
+ print("List of Students:")
 
-print("--- Entrance Exam Students ---")
-cet_students = {"Alice", "Bob", "Charlie", "David"}
-jee_students = {"Eve", "Frank", "Grace", "Heidi"}
-neet_students = {"Ivan", "Judy", "Karl", "Liam"}
+cet = {"Alice", "Bob", "Charlie", "Frank"}
+jee = {"Bob", "Eve", "Frank", "Heidi"}
+neet = {"Bob", "Charlie", "Karl", "Liam"}
 
-print(f"List of Students:")
-print(f"CET Students: {cet_students}")
-print(f"JEE Students: {jee_students}")
-print(f"NEET Students: {neet_students}")
+print(f"CET Students: {cet}")
+print(f"JEE Students: {jee}")
+print(f"NEET Students: {neet}")
+
+all_students = cet | jee | neet
+print(f"All Students: {all_students}")
+
+all_exams = cet & jee & neet
+print(f"All Exams: {all_exams}")
+
+jee_not_neet = jee - neet
+print(f"JEE but not for NEET: {jee_not_neet}")
+
+cet_and_neet_not_jee = (cet & neet) - jee
+print(f"CET and NEET but not for JEE: {cet_and_neet_not_jee}")
 
 # Write your code here
 # TODO: Find and Print All the Students appearing for any Entrance Exam
